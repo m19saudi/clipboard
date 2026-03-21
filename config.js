3759e9f7e418db061507f38b55a3a02ca4aaad0a
m19saudi <269192418+m19saudi@users.noreply.github.com>
2026-03-21 20:41:34 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// This file contains your connection keys.
+// Even if seen, they are restricted to ONLY work on your website.
+const firebaseConfig = {
+    apiKey: "AIzaSyAVd3W7MFt6Ddq79WwxkaJ_lGTW9Hig08c",
+    authDomain: "quick-clipboard.firebaseapp.com",
+    databaseURL: "https://quick-clipboard-default-rtdb.firebaseio.com",
+    projectId: "quick-clipboard",
+    storageBucket: "quick-clipboard.firebasestorage.app",
+    messagingSenderId: "292937997761",
+    appId: "1:292937997761:web:ae94b22dc263545bf86340"
+};
