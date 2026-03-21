4ff89f3f99c0d89bc7b10b3a0774454599821e09
m19saudi <269192418+m19saudi@users.noreply.github.com>
2026-03-21 20:51:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 // This file contains your connection keys.
 // Even if seen, they are restricted to ONLY work on your website.
 const firebaseConfig = {
-    apiKey: "AIzaSyAVd3W7MFt6Ddq79WwxkaJ_lGTW9Hig08c",
-    authDomain: "quick-clipboard.firebaseapp.com",
-    databaseURL: "https://quick-clipboard-default-rtdb.firebaseio.com",
-    projectId: "quick-clipboard",
-    storageBucket: "quick-clipboard.firebasestorage.app",
-    messagingSenderId: "292937997761",
-    appId: "1:292937997761:web:ae94b22dc263545bf86340"
+  apiKey: "AIzaSyDh8uvlYT1tCIoYiHeqOZ2lwYiOKbqv3aQ",
+  authDomain: "my-quick-clip2.firebaseapp.com",
+  projectId: "my-quick-clip2",
+  storageBucket: "my-quick-clip2.firebasestorage.app",
+  messagingSenderId: "499890221863",
+  appId: "1:499890221863:web:94d67c4a23ec2a1f4eef02"
 };
